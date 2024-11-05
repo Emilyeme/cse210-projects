e0a12571820eff7347279e2ad3b1f48915804483
Emilyeme <emilyemerson395@gmail.com>
2024-11-05 09:46:31 -0800

    grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage:  ");
+        string answer= Console.ReadLine();
+        int percentage = int .Parse(answer);
+        String letter="";
+
+        if (percentage>=90)
+        {
+            letter="A";
+        }
+        else if( percentage>=80){
+            letter="B";
+        }
+
+        else if(percentage>=70){
+            letter="C";
+        }
+        else if( percentage>=60){
+            letter="D";
+        }
+        else if (percentage<60)
+        {
+           letter="F"; 
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (percentage>=70){
+            Console.WriteLine("Your have passed congs!!!!");
+        }
+
+        else{
+            Console.WriteLine("Pull up your stockings better nest time.");
+        }
+        
+        
+            
     }
 }
