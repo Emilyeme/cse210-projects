77a059329768133a6e995b477b1091041169dc9a
Emilyeme <emilyemerson395@gmail.com>
2024-11-09 16:07:20 -0800

    functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName= PromptUserName();
+        int userNumber= PromptUserNumber();
+        int squaredNumber=SuquareNumber(userNumber);
+        DisplayResults(userName,squaredNumber);
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to our Program.");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name ?");
+        string name=Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favourite number: ");
+        int number= int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SuquareNumber(int number)
+    {
+        int square= number * number;
+        return square;
+
+    }
+
+    static void DisplayResults(string name, int square)
+    {
+        Console.WriteLine($"{name},the square of your number is {square}");
     }
 }
