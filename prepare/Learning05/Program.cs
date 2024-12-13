c8bb1a474e7e67774e6b06a2f9d2d7208f17a17b
Emilyeme <emilyemerson395@gmail.com>
2024-12-13 10:00:35 -0800

    assign

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+      Assignment a1 =new Assignment("Emily Emerson", "Abstraction");
+      Console.WriteLine(a1.GetSummary()); 
+
+      MathAssignment a2 = new MathAssignment("Edrinah Nels", "Indexes", "10", "2-8");
+      Console.WriteLine(a2.GetSummary());
+      Console.WriteLine(a2.GetHomeworkList());
+
+      WritingAssignment a3 = new WritingAssignment("Myhra Blessing", "Designs","Web and computer programming");
+      Console.WriteLine(a3.GetSummary());
+      Console.WriteLine(a3.GetWritingInformation());
+
     }
 }
